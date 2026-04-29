026489b5b91cda19418f70aa2fa651b93f299e14
Joshua Pham <joshpham@chapman.edu>
2026-04-29 10:23:30 -0700

    Created player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour 
+{
+	
+	void Awake()
+	{
+		RangeIndicator rangeIndicator = GetComponent<RangeIndicator>();
+		rangeIndicator.Initialize(GameParameters.PlayerAttackRange);
+	}
+}
