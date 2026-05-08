c5507de1ac3fd169a7cc25cffe6afdb5fa2d5ef7
Joshua Pham <joshpham@chapman.edu>
2026-05-08 00:38:24 -0700

    Removed projectile wiggle

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,17 +2,13 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public float WiggleSpeed = 5f;
-    public float WiggleAmount = 0.02f;
-    
     private Transform Target;
 
     private float AttackDamage;
     private float RotateSpeed = 500f;
     private float Radius = 0.5f;
-    private float WiggleTime;
 
-    public void Initialize(Transform target, float damage)
+    public void Initialize(Transform target,  float damage)
     {
         Target = target;
         AttackDamage = damage;
@@ -32,10 +28,6 @@ public class Projectile : MonoBehaviour
         transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, RotateSpeed * Time.deltaTime);
 
         transform.position += transform.forward * GameParameters.ProjectileSpeed * Time.deltaTime;
-
-        WiggleTime += Time.deltaTime;
-        Vector3 wiggle = transform.right * Mathf.Sin(WiggleTime * WiggleSpeed) * WiggleAmount;
-        transform.position += wiggle * Time.deltaTime;
 
         if (Vector3.Distance(transform.position, Target.position) <= Radius)
         {
